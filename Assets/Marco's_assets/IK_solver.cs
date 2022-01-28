f39a174cd32b63e29c48bace9f9a33070988e47e
SpaceMarco <black1cl0ud99@gmail.com>
2022-01-28 18:37:41 +0200

    finished procedural animation!

---
+++ Assets/Marco's_assets/IK_solver.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IK_solver : MonoBehaviour
+{
+    public Transform body;
+    public LayerMask groundLayer;
+    public float speed;
+    public IK_solver otherFoot;
+    [SerializeField]float footSpacing;
+    [SerializeField]float stepDistance = 4;
+    [SerializeField]float stepLength = 4;
+    [SerializeField]float stepHeight = 4;
+    Vector3 footOffset = Vector3.zero;
+    Vector3 newPosition, oldPosition, currentPosition;
+    Vector3 oldNormal,currentNormal,newNormal;
+    float lerp;
+
+    private void Start()
+    {
+        footSpacing = transform.localPosition.x;
+        currentPosition = newPosition = oldPosition = transform.position;
+        currentNormal = newNormal = oldNormal = transform.up;
+        lerp = 1;
+    }
+
+    void Update()
+    {
+        transform.position = currentPosition;
+        transform.up = currentNormal;
+        Ray ray = new Ray(body.position + (body.right * footSpacing), Vector3.down);
+        if (Physics.Raycast(ray, out RaycastHit info, 10, groundLayer.value))
+        {
+            if (Vector3.Distance(newPosition, info.point) > stepDistance && !otherFoot.IsMoving() && lerp>=1)
+            {
+                lerp = 0;
+                int direction = body.InverseTransformPoint(info.point).z > body.InverseTransformPoint(newPosition).z ? 1 : -1;
+                newPosition = info.point + (body.forward * stepLength * direction)+footOffset;
+                newNormal = info.normal;
+            }
+        }
+
+        if (lerp < 1)
+        {
+            Vector3 tempPos = Vector3.Lerp(oldPosition, newPosition, lerp);
+            tempPos.y += Mathf.Sin(lerp * Mathf.PI) * stepHeight;
+
+            currentPosition = tempPos;
+            currentNormal = Vector3.Lerp(oldNormal, newNormal, lerp);
+            lerp += Time.deltaTime * speed;
+        }
+        else
+        {
+            oldPosition = newPosition;
+            oldNormal = newNormal;
+        }
+    }
+
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.red;
+    //    Gizmos.DrawSphere(newPosition,0.5f);
+    //}
+
+    public bool IsMoving()
+    {
+        return lerp < 1;
+    }
+}
+
